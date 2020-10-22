6f71a752d6b340d22cb9370035121aa54179e818
Tate Wilhelm <tatewilhelm@yahoo.com>
2020-10-21 23:45:17 -0400

    Connect app to api

--- LineupApp/src/components/Settings.js
+++ LineupApp/src/components/Settings.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import {lineupService} from '../services/LineupService';
+import {Dropdown} from 'primereact/dropdown';
 
 export class Settings extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            teams: [],
+            selectedTeam: {}
+        };
+    }
+
+    componentDidMount = async () => {
+        var teams = await lineupService.getTeams();
+        this.setState({
+            teams: teams
+        });
+        console.log(teams);
+    }
 
     render() {
         return(
             <div>
                 <h1>Settings</h1>
+                <Dropdown options={this.state.teams} optionLabel="name" value={this.state.selectedTeam} onChange={(e) => {this.setState({selectedTeam: e.value})}}/>
             </div>
         );
     }
--- LineupApp/src/services/LineupService.js
+++ LineupApp/src/services/LineupService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const lineupService = {
-    getTeamSchedule
+    getTeamSchedule,
+    getTeams
 }
 
 async function getTeamSchedule(teamAbv) {
@@ -9,6 +10,20 @@ async function getTeamSchedule(teamAbv) {
         const response = await axios({
             method: 'GET',
             url: 'http://site.api.espn.com/apis/site/v2/sports/football/college-football/teams/' + teamAbv + '/schedule'
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function getTeams()
+{
+    try {
+        const response = await axios({
+            method: 'GET',
+            url: 'http://localhost:9000/api/lineup/teams'
         });
         return response.data;
     } catch (error) {
